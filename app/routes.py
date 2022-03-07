560ddd89223fa0cf0a64a0de8e19fb698e490e45
Tomas-Pytel <Tomas.Pytel@ibm.com>
2022-03-07 13:58:55 +0100

    Add endpoints without auth

--- app/routes.py
+++ app/routes.py
@@ -21,6 +21,11 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
+@app.route("/")
+def index():
+    return redirect(url_for("items_index"))
+
+
 @app.route("/login", methods=["GET", "POST"])
 @templated()
 def login():
@@ -38,12 +43,6 @@ def logout():
     logout_user()
     flash("You have been logged out.", "success")
     return redirect(url_for("login"))
-
-
-@app.route("/")
-@login_required
-def index():
-    return redirect(url_for("feeds_articles"))
 
 
 @app.route("/feeds", methods=["GET", "POST"])
@@ -105,10 +104,12 @@ def feeds_download(feed_id):
 
 @app.route("/feeds/refresh", methods=["GET"])
 def feeds_refresh():
+    import time
     feeds = Feed.get_all_feeds()
     articles_list = []
     for feed in feeds:
         articles = download_articles(feed.url, feed.id)
+        time.sleep(10)
         articles_list += articles
 
     return "".join(articles_list)
@@ -141,3 +142,32 @@ def search():
         articles = Article.get_all_articles()
 
     return dict(articles=articles, search_query=search_query)
+
+
+@app.route("/items", defaults={"feed_id": None}, methods=["GET"])
+@app.route("/items/<feed_id>/")
+@templated("items_index.html")
+def items_index(feed_id):
+    if feed_id is None:
+        articles = Article.get_last_articles()
+        feed_name = "All"
+    else:
+        feed_name = Feed.get_feed_by_id(feed_id)[0].name
+        articles = Article.get_articles_by_feed_id(feed_id)
+    return dict(articles=articles, feeds=Feed.get_all_feeds(), feed_name=feed_name)
+
+
+@app.route("/items-search", methods=["GET", "POST"])
+@templated("items_index.html")
+def items_search():
+    if request.method == "POST":
+        form = SearchForm()
+        search_query = form.data["article_text"]
+        articles = Article.query.filter(
+            Article.summary.ilike(f"%{search_query}%")
+        ).all()
+    else:
+        search_query = False
+        articles = Article.get_all_articles()
+
+    return dict(articles=articles, search_query=search_query, feeds=Feed.get_all_feeds())
